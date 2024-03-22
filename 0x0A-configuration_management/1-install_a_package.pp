74ba7e6120913627d1773bb5e78110c62ab4c9b8
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-03-22 08:59:56 -0700

    install package flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install flask from pip3
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
