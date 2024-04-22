d60b00f01a18e6a1a709d7c1a21170e112ec339c
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-04-22 13:41:49 -0700

    export_to_JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to JSON format."""
+"""
+Export to JSON
+"""
+
 import json
 import requests
 import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
+
     url = "https://jsonplaceholder.typicode.com/"
+
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
