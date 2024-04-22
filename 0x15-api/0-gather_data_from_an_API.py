570518958759a8d7c0499d6bb60c4c92233c84b3
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-04-22 11:14:46 -0700

    Gather data from api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,28 +8,11 @@ import sys
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    """user = requests.get(reqs + "users/{}".format(sys.argv[1])).json()
+    reqs = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(reqs + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(reqs + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{})".format(
         user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]"""
-
-    employee_id = sys.argv[1]
-    user = requests.get(url + "users/{}".format(employee_id)).json()
-
-    # Get the to-do list for the employee using the provided employee ID
-    params = {"userId": employee_id}
-    todos = requests.get(url + "todos", params).json()
-
-    # Filter completed tasks and count them
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-
-    # Print the employee's name and the number of completed tasks
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-
-    # Print the completed tasks one by one with indentation
-    [print("\t {}".format(complete)) for complete in completed]
+    [print("\t {}".format(c)) for c in completed]
