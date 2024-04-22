2167d8c1f0e7f77237a52fd66c34b95e2f16661a
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-04-22 13:16:32 -0700

    one

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-""" Gather data from an API
-using this REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-"""
+""" Gather data from an API """
 import requests
 from sys import argv
 
