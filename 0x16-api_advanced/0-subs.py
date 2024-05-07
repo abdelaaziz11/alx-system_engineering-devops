5c1f939847830786fa27bba54250b231b134575d
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-05-07 11:40:25 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,8 +2,15 @@
 '''
 Queries the Reddit API and returns the number of subscribers
 '''
+import request
+
 
 def number_of_subscribers(subreddit):
     ''' returns the number of subscribers '''
-    if not subreddit:
+    if subreddit is None or type(subreddit) in not str:
         return 0;
+    req = request.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+            headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+            subscribes = req.get("data", {}).get("subscribers", 0)
+            return subscribes
